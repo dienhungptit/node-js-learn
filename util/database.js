4d6b39d35f75f1665d198da5a3bffae124bc0c93
Lê Phúc Diên Hưng <hunglpd@sphoton.com>
2021-07-07 12:00:19 +0700

    suarw

--- util/database.js
+++ util/database.js
@@ -1,10 +1,10 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'nodecomplete'
+  host: "localhost",
+  user: "root",
+  database: "node-complete",
+  password: "12345678",
 });
 
 module.exports = pool.promise();
