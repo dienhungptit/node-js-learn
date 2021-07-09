2c755cc9ceb94b1a81ddf5449d9ca49281059280
Lê Phúc Diên Hưng <hunglpd@sphoton.com>
2021-07-09 17:07:37 +0700

    sửa

--- models/user.js
+++ models/user.js
@@ -1,3 +1,21 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  cart: {
+    items: [
+      {
+        productId: { type: Schema.Types.ObjectId, required: true },
+        quantity: { type: Number, required: true },
+      },
+    ],
+  },
+});
+
+module.exports = userSchema;
+
 // const mongodb = require('mongodb');
 // const getDb = require('../util/database').getDb;
 
